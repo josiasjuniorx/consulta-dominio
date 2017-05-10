a4e8303d8a1ba249a9c3882835956d7e72ec104d
josiasjuniorx <josiasjuniorx@gmail.com>
2017-05-09 21:50:28 -0300

    Change local_settings allow all

--- consultas/local_settings.py
+++ consultas/local_settings.py
@@ -2,8 +2,8 @@
 SECRET_KEY = 'q8j#q63l#$62o#q5%6)695!@n1&2e+m310ojehsm$7po1ycfg3z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 ADMIN_ENABLED = False
