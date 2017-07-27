9ed1e0afe9c395d1b130b43860a1a7e5f80f09d7
Junior <junior@debian>
2017-07-27 20:48:51 -0300

    Add whois.conf and adjusts on searchstrings

--- dominio/searchstrings.py
+++ dominio/searchstrings.py
@@ -1,4 +1,4 @@
-nameserver = r'(?<= Name Server:).*|(?<=nserver:).*'
+nameserver = r'(?<=Name Server:).*|(?<=nserver:).*'
 
 www = r'(?<=has address ).*|(?<=has IPv6 Address ).*|(?<=domain name pointer ).*(?=.)'
 
@@ -18,33 +18,8 @@ soa = r'(?<= has SOA record ).*'
 
 ptr = r'(?<= domain name pointer ).*(?=.)'
 
-# status = r'(?<=status: ).*'
-status = \
-r'.*status:.*|\
-Registration status:.*|\
-state:.*'
+status = r'.*status:.*|Registration status:.*|state:.*'
 
-expiration_date = \
-r'(?<!Registration) Expiration Date:.*(?!Z)|\
-expires.*|\
-expiry date:.*|\
-Renewal date:.*|\
-\npaid-till:.*|\
-\[Expires on\].*|\
-\[State\].*|\
-Domain Expiration Date:.*|\
-domain_datebilleduntil:.*|\
-expire:.*|\
-Expire Date:.*'
+expiration_date = r'(?<!Registration) Expiration Date:.*(?!Z)|expires.*|expiry date:.*|Registry Expiry Date:.*|Renewal date:.*|\npaid-till:.*|\[Expires on\].*|\[State\].*|Domain Expiration Date:.*|domain_datebilleduntil:.*|expire:.*|Expire Date:.*'
 
-owner = \
-r'registrant.*:.*|\
-provider:.*|\
-.*Registrar:.*|\
-Company Name:.*|\
-registrar_name:.*|\
-owner.*|\
-responsible:.*|\
-query_status:.*|\
-domain:.*|\
-.*domain name:.*'
+owner = r'registrant.*:.*|provider:.*|.*Registrar:.*|Company Name:.*|registrar_name:.*|owner.*|responsible:.*|query_status:.*|domain:.*|.*domain name:.*'
