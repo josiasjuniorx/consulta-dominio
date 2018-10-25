74b7e5830a38e5b46b895825acb9a937ecb84699
Josias Junior <josias.junior@localhost.localdomain>
2018-10-25 18:41:14 -0300

    alter debug to true to openshift

--- consultas/local_settings.py
+++ consultas/local_settings.py
@@ -2,7 +2,7 @@
 SECRET_KEY = 'q8j#q63l#$62o#q5%6)695!@n1&2e+m310ojehsm$7po1ycfg3z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
